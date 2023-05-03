43d6cabd2ab4535e1c89b3c6bd7e9ec8cbf2e53f
kgil60 <gilvaryke@gmail.com>
2023-05-03 01:22:07 -0400

    style login form

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -8,11 +8,11 @@ export default class LoginForm extends React.Component {
                 <form className='login-form'>
                     <div className='input-container'>
                         <label for='username' className='input-label'>Username</label>
-                        <input name='username' id='username' type='text' placeholder='Input username here'></input>
+                        <input className='form-input' name='username' id='username' type='text' placeholder='Input username here'></input>
                     </div>
                     <div className='input-container'>
                         <label for='password' className='input-label'>Password</label>
-                        <input name='password' id='password' type='password' placeholder='Input password here'></input>
+                        <input className='form-input' name='password' id='password' type='password' placeholder='Input password here'></input>
                     </div>
                     <div className='button-container'>
                         <button type='submit' className='submit-button'>Log In</button>
